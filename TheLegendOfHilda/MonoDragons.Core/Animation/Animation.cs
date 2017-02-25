d033386762d2d5e7bb6db25c8e579bb043951ce7
Brendan H <bdharwood@live.com>
2017-02-25 10:43:27 -0700

    Initial animation class

---
+++ TheLegendOfHilda/MonoDragons.Core/Animation/Animation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace MonoDragons.Core.Animation
+{
+    public class Animation
+    {
+        Texture2D texture;
+        List<Rectangle> frames;
+        int frameNum;
+        double secondsPerFrame;
+        double currentSeconds = 0;
+
+        public Animation(Texture2D texture, List<Rectangle> frames, double secondsPerFrame)
+        {
+            this.texture = texture;
+            this.frames = frames;
+            this.secondsPerFrame = secondsPerFrame;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Render(SpriteBatch spriteBatch, Vector2 location)
+        {
+            spriteBatch.Draw(texture, location, frames[frameNum], Color.White);
+        }
+
+        public void Reset()
+        {
+            frameNum = 0;
+        }
+    }
+}
