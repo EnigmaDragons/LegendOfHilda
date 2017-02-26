2b82448b5dbbaf582e491618b15ffb7075cebe56
Giovanni Aratico <Giovanni Aratico>
2017-02-25 21:08:49 -0700

    Credits Functional

--- TheLegendOfHilda/TheLegendOfHilda/Program.cs
+++ TheLegendOfHilda/TheLegendOfHilda/Program.cs
@@ -19,7 +19,7 @@ namespace TheLegendOfHilda
         [STAThread]
         static void Main()
         {
-            using (var game = new MainGame("MainHallRoom", new ScreenSize(1344, 960), 3, CreateSceneFactory(), CreateKeyboardContoller()))
+            using (var game = new MainGame("Victory", new ScreenSize(1344, 960), 3, CreateSceneFactory(), CreateKeyboardContoller()))
                 game.Run();
         }
 
