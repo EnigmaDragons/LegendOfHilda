4ee2d8fd05bb6aff79fcb3365b19a05da884c837
Silas Reinagel <silas.reinagel@gmail.com>
2017-02-25 17:36:59 -0700

    Added spear enemy

---
+++ TheLegendOfHilda/TheLegendOfHilda/Enemies/SpearEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using MonoDragons.Core.Animation;
+using MonoDragons.Core.Engine;
+using TheLegendOfHilda.PlayerStuff;
+
+namespace TheLegendOfHilda.Enemies
+{
+    public class SpearEnemy : Character, IVisualAutomaton
+    {
+        private Dictionary<AnimationState, Animation> _animations;
+
+        public SpearEnemy()
+            : base(CreateAnimations())
+        {
+        }
+
+        public void Update(TimeSpan delta)
+        {
+            base.Update(delta);
+        }
+
+        public void Draw(Vector2 offset)
+        {
+        }
+
+        private static Dictionary<AnimationState, Animation> CreateAnimations()
+        {
+            var animations = new Dictionary<AnimationState, Animation>();
+            animations[AnimationState.WalkingForward] = new StillFrame("Images/Enemies/striker-u", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.WalkingBackward] = new StillFrame("Images/Enemies/striker-d", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.WalkingLeft] = new StillFrame("Images/Enemies/striker-l", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.WalkingRight] = new StillFrame("Images/Enemies/striker-r", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.StandingForward] = new StillFrame("Images/Enemies/striker-u", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.StandingBackward] = new StillFrame("Images/Enemies/striker-d", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.StandingLeft] = new StillFrame("Images/Enemies/striker-l", new Rectangle(0, 0, 32, 32));
+            animations[AnimationState.StandingRight] = new StillFrame("Images/Enemies/striker-r", new Rectangle(0, 0, 32, 32));
+            return animations;
+        }
+    }
+}
