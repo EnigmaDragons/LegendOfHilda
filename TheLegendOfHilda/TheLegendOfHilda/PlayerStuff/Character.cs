eed0ae4d934a9ab63509f42ebcdad284347415ea
Silas Reinagel <silas.reinagel@gmail.com>
2017-02-25 22:52:55 -0700

    Sword sound on attack

--- TheLegendOfHilda/TheLegendOfHilda/PlayerStuff/Character.cs
+++ TheLegendOfHilda/TheLegendOfHilda/PlayerStuff/Character.cs
@@ -118,6 +118,7 @@ namespace TheLegendOfHilda.PlayerStuff
                     currentAnimationState == AnimationState.WalkingLeft)
                     currentAnimationState = AnimationState.AttackingLeft;
                 isAttacking = true;
+                World.PlaySound("SFX/Sword1");
                 currentAttackTime = attackTime;
             }
         }
